8ce305c89e5b931b8bacc4da458104cfdb43cfad
Satyabrat Ojha <satyabratojha04@gmail.com>
2024-06-05 10:54:20 +0530

    Time: 11 ms (51%), Space: 15.9 MB (39.16%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,48 +1,18 @@
-class Node {
-public:
-    Node *child[26];
-    bool isWord;
-    Node() {
-        for(auto &c:child) c=nullptr;
-        isWord=0;
-    }
-};
-
 class Solution {
 public:
-    Node* root;
-    vector<bool> dp;
-
     bool wordBreak(string s, vector<string>& wordDict) {
-        insertDict(wordDict);
-        dp.clear();
-        dp.resize(s.size() + 1, 0);
-        return findWord(s,root);
-    }
+        int n = s.size();
+        vector<bool> dp(n);
+        unordered_set<string> st;
+        for(auto word:wordDict) st.insert(word);
 
-    bool findWord(string s, Node* tail) {
-        if(s.empty()) return tail->isWord;
-        if(root == tail && dp[s.size()] == 1) return 0;
-
-        if(tail->isWord && findWord(s,root)) return 1;
-
-        int i = s[0] - 'a';
-        if(tail->child[i] && findWord(s.substr(1), tail->child[i])) return 1;
-
-        if(root == tail) dp[s.size()] = 1;
-        return 0;
-    }
-
-    void insertDict(vector<string>& wordDict) {
-        root = new Node();
-        for(auto word:wordDict) {
-            auto tail = root;
-            for(auto c:word) {
-                int i = c - 'a';
-                if(!tail->child[i]) tail->child[i] = new Node();
-                tail = tail->child[i];
-            }
-            tail->isWord = 1;
+        for(int i=0;i<n;i++) {
+            if(i!=0 && dp[i-1]==0) continue;
+            for(int j=1;i+j-1<n;j++)
+                if(st.find(s.substr(i,j)) != st.end())
+                    dp[i+j-1] = 1;
         }
+
+        return dp[n-1];
     }
 };
